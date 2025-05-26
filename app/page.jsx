9a82c195497b8d89673eea4e0f8800fe390beeba
Vibhanshu Pandey <vibhanshu2207@gmail.com>
2025-05-27 02:52:05 +0530

    as

--- app/page.jsx
+++ app/page.jsx
@@ -1,22 +1,13 @@
 "use client";
-import { useState } from "react";
 import { HomeScreen } from "@components/HomeScreen";
 
 const Home = () => {
-  const [input, setInput] = useState("");
-  const [showEditor, setShowEditor] = useState(false);
-
-  const toggleEditor = () => {
-    setInput("");
-    setShowEditor(!showEditor);
-  };
-
   return (
     <>
       <main
         className={`h-screen flex flex-col items-center justify-center transition-all duration-500 ease-in-out bg-bgGray text-slate-200`}
       >
-        <HomeScreen toggleEditor={toggleEditor} />
+        <HomeScreen />
       </main>
     </>
   );
